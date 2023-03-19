42c2cac13166e692ce31084c676d24954e2e63cc
Niatomi <playervoker@gmail.com>
2023-03-20 02:40:46 +0400

    Add operations metadata

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,12 +1,14 @@
-from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
-from src.config import DB_HOST, DB_PORT, DB_PASS, DB_USER, DB_NAME
-from src.models import Base
-
 from alembic import context
+import sys
+import os
+from sqlalchemy import pool
+from sqlalchemy import engine_from_config
+from logging.config import fileConfig
+sys.path.append(os.path.join(sys.path[0], 'src'))
+from src.config import DB_HOST, DB_PORT, DB_PASS, DB_USER, DB_NAME
+
+from src.auth.models import Base as auth_base
+from src.operations.models import Base as operations_base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,7 +28,7 @@ config.set_section_option(section, 'DB_NAME', DB_NAME)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = [auth_base.metadata, operations_base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
