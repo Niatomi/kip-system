dfba22df534a47c554854615551e088d1810d478
Niatomi <playervoker@gmail.com>
2023-04-26 09:41:55 +0400

    Initial commit

--- backend/src/operations/router.py
+++ backend/src/operations/router.py
@@ -21,19 +21,19 @@ router = APIRouter(
     tags=['operations']
 )
 
+
 @router.get('/get_specific_operation')
 async def get_specific_operation(operation_type, session: AsyncSession = Depends(get_async_session)):
     query = select(Operation).where(Operation.type == operation_type)
     result = await session.execute(query)
     return [el[0] for el in result.all()]
 
+
 @router.post('/insert_operation', status_code=201)
 async def add_specific_operation(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
-    # new_operation_db = Operation(new_operation.from_orm(Operation))
-    # print(new_operation_db)
     statement = insert(Operation).values(**new_operation.dict())
     session.add(Operation(**new_operation.dict()))
-    
+
     try:
         await session.commit()
         return 'Data saved'
