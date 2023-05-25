5e553abce410ed34c2a1a05deb8d5f569d23ca51
Niatomi <playervoker@gmail.com>
2023-05-25 22:09:39 +0400

    Changed pydantic and add roles

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,5 +1,12 @@
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
+from enum import Enum
+
+
+class Roles(str, Enum):
+    admin = 'ADMIN'
+    chief = 'CHIEF'
+    worker = 'WORKER'
 
 
 class Users(models.Model):
@@ -17,6 +24,8 @@ class Users(models.Model):
     second_name = fields.CharField(max_length=50, null=True)
     third_name = fields.CharField(max_length=50, null=True)
 
+    role = fields.CharEnumField(Roles, null=True, default=Roles.worker)
+
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
 
@@ -33,12 +42,12 @@ class Users(models.Model):
 
 UserCredentialsPydantic = pydantic_model_creator(Users,
                                                  name="User",
-                                                 exclude=['password_hash',
-                                                          'email',
-                                                          'id',
-                                                          'created_at',
-                                                          'modified_at'])
+                                                 include=[
+                                                     'username',
+                                                     'first_name',
+                                                     'second_name',
+                                                     'third_name'
+                                                 ])
 User_Pydantic = pydantic_model_creator(Users,
-                                       name="User",
-                                       exclude=['password_hash'])
+                                       name="User")
 UserIn_Pydantic = pydantic_model_creator(Users, name="UserIn", exclude_readonly=True)
