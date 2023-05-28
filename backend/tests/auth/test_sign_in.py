2822278333f97aceb1253067ea644cf458344de2
Niatomi <playervoker@gmail.com>
2023-05-28 22:46:47 +0400

    Fixed test config

--- backend/tests/auth/test_sign_in.py
+++ backend/tests/auth/test_sign_in.py
@@ -1,13 +1,21 @@
 import pytest
 from httpx import AsyncClient
-from tortoise.backends.base.client import BaseDBAsyncClient
+from src.models import Users
 
 
-@pytest.mark.asyncio
-async def test_get_something(async_client: AsyncClient, conn: BaseDBAsyncClient) -> None:
-    # await conn.execute_query
-    # response = await async_client.get("/something?skip=0&limit=5")
+@pytest.mark.anyio
+async def test_testpost(client: AsyncClient):
+    name, age = ["sam", 99]
+    assert 1 == 1
+    # assert await User.filter(username=name).count() == 0
 
-    # assert response.status_code == status.HTTP_200_OK
-    # assert 0 < len(response.json()) <= 5
-    pass
+    # data = {"username": name, "age": age}
+    # response = await client.post("/testpost", json=data)
+    # assert response.json() == dict(data, id=1)
+    # assert response.status_code == 200
+
+    # response = await client.get("/users")
+    # assert response.status_code == 200
+    # assert response.json() == [dict(data, id=1)]
+
+    # assert await User.filter(username=name).count() == 1
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,25 +1,54 @@
-from typing import Generator
+import asyncio
 import pytest
 from httpx import AsyncClient
-from asyncio import get_event_loop
+from tortoise import Tortoise
 
 from src.main import app
+from src.database import db_url
 
-from tortoise import Tortoise
+DB_URL = db_url
+
+
+async def init_db(db_url, create_db: bool = False, schemas: bool = False) -> None:
+    """Initial database connection"""
+    await Tortoise.init(
+        db_url=db_url, modules={"models": ["models"]}, _create_db=create_db
+    )
+    if create_db:
+        print(f"Database created! {db_url = }")
+    if schemas:
+        await Tortoise.generate_schemas()
+        print("Success to generate schemas")
+
+
+async def init(db_url: str = DB_URL):
+    await init_db(db_url, True, True)
 
 
 @pytest.fixture(scope="session")
-def conn() -> Generator:
-    yield Tortoise.get_connection("default")
+def anyio_backend():
+    return "asyncio"
 
 
-@pytest.fixture(scope="module")
-async def async_client() -> Generator:
-    async with AsyncClient(app=app, base_url="http://testserver") as client:
+@pytest.fixture(scope="session")
+async def client():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        print("Client is ready")
         yield client
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session", autouse=True)
+async def initialize_tests():
+    await init()
+    yield
+    await Tortoise._drop_databases()
+
+
+
+@pytest.fixture(scope="session")
 def event_loop():
-    loop = get_event_loop()
+    """Overrides pytest default function scoped event loop"""
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
     yield loop
+    loop.close()
