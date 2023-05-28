66f1f48eb931f7da4b15aa6445bb589af66ba157
Niatomi <playervoker@gmail.com>
2023-05-28 23:47:59 +0400

    Initial commit

---
+++ frontend/src/components/main-wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-main-wrapper">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vMainWrapper",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
