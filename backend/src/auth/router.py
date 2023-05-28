2996ca78203efea3ae100e48a6c6fc66896bfb9c
Niatomi <playervoker@gmail.com>
2023-05-28 21:02:10 +0400

    Add transaction on sign_up

--- backend/src/auth/router.py
+++ backend/src/auth/router.py
@@ -8,6 +8,7 @@ from fastapi import APIRouter
 from fastapi import Depends
 from tortoise.expressions import Q
 from . import schemas as auth_schemas
+from tortoise.transactions import in_transaction
 
 from . import utils
 from .oauth2 import (
@@ -38,14 +39,18 @@ async def sign_in(user_credentials: OAuth2PasswordRequestForm = Depends()):
 
 @router.post('/sign_up')
 async def sign_up(sign_up_user: UserCredentials):
+
     user = await Users.filter(Q(username=sign_up_user.username) |
                               Q(email=sign_up_user.email)).first()
+
     if user is not None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User already exists')
     sign_up_user = sign_up_user.dict()
     sign_up_user['password_hash'] = hash(sign_up_user.pop('password'))
 
-    await Users.create(**sign_up_user)
+    async with in_transaction():
+        await Users.create(**sign_up_user)
+
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=None)
 
 
@@ -58,7 +63,6 @@ async def refresh_token(token_schema: auth_schemas.UserToken):
     user_id = verify_access_token(
         token=token_schema.access_token,
         credentials_exception=credentials_exception)
-    print(user_id)
 
     user_token = auth_schemas.UserToken()
     user_token.access_token = create_access_token({
