9731b2acd855040e5a33f301824ef615749b9e4b
Niatomi <playervoker@gmail.com>
2023-05-30 02:53:37 +0400

    Change mongo connect via env

--- backend/src/database.py
+++ backend/src/database.py
@@ -9,16 +9,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 mongo_url = 'mongodb://' + \
     f'{mongo_db_config.mongo_username}:{mongo_db_config.mongo_password}@' + \
     f'localhost:27017/{mongo_db_config.database_name}?retryWrites=true&w=majority'
-
+print(mongo_url)
 
 client = AsyncIOMotorClient(mongo_url)
-db = client.kip_system_db
+db = client[f'{mongo_db_config.database_name}']
 
 db_url = 'postgres://' + \
     f'{db_config.db_user}:{db_config.db_pass}@' + \
     f'{db_config.db_host}:{db_config.db_port}/{db_config.db_name}'
 models = ["src.models", "src.devices.models", "aerich.models"]
-
 TORTOISE_ORM = {
     "connections": {"default": db_url},
     "apps": {
