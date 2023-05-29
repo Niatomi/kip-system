724a47bf534ee618e63f19f35ed381c535231f1d
Niatomi <playervoker@gmail.com>
2023-05-30 02:54:49 +0400

    implemented crud funcs

--- backend/src/devices/device_pool/router.py
+++ backend/src/devices/device_pool/router.py
@@ -1,63 +1,79 @@
-from src.database import client as mongo_client
+from bson.objectid import ObjectId
+from tortoise.expressions import Q
 from src.database import db
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi import status
 from fastapi.exceptions import HTTPException
-from src.utils import get_user_role
 from fastapi import APIRouter
 from fastapi import Depends
 
-from src.auth.oauth2 import get_current_user
-from src.models import Roles
 from .. import models
 from . import schemas
+from src import utils
 from tortoise.transactions import in_transaction
 
 router = APIRouter(prefix='/device_pool',
-                   dependencies=[Depends(get_current_user)],
+                   dependencies=[Depends(utils.check_user_is_not_worker)],
                    tags=['Admin', 'Chief'])
 
 
 @router.post('/')
 async def add_device_into_pool(device_specs: schemas.DevicePoolPost,
-                               device_info: models.DevicePoolPydantic,
-                               role: Roles = Depends(get_user_role)):
-    if role != Roles.admin:
-        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                            detail="User don't have permissions")
+                               device_info: models.DevicePoolPydantic):
 
     async with in_transaction():
-        async with mongo_client.start_session as s:
-            async with s.start_transaction():
-                new_device_info = await db.device_description.insert_one(
-                    jsonable_encoder(device_specs)
-                )
         device_info = device_info.dict()
-        device_info['mongo_id'] = new_device_info.inserted_id
+        device_info['mongo_id'] = 'None'
+        await models.DevicesPool.create(**device_info)
 
-        await models.DevicesPool.create(**device_info.dict())
+        new_device_info = await db.device_description.insert_one(
+            jsonable_encoder(device_specs)
+        )
+        device_info['mongo_id'] = new_device_info.inserted_id
+        await models.DevicesPool.filter(name=device_info.pop('name')).update(**device_info)
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=None)
 
 
-@router.get('/')
-async def get_device_by_name(device_specs: schemas.DevicePoolPost,
-                             device_info: models.DevicePoolPydantic,
-                             role: Roles = Depends(get_user_role)):
-    # if role != Roles.admin:
-    #     raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-    #                         detail="User don't have permissions")
+@router.get('/device')
+async def get_device_by_params(
+        params: schemas.DevicePoolGetRequest = Depends(schemas.DevicePoolGetRequest)):
+    if params.dict(exclude_none=True) == {}:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='At least one params required')
+    return await models.DevicesPool.filter(Q(*params.generate_exporession(),
+                                             join_type='OR')).first()
+
+
+@router.get('/device/{device_id}')
+async def get_device_by_id(device_id: str):
+
+    pg_device = await models.DevicesPool.filter(id=device_id).first()
+    if pg_device is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Device not found')
+    mongo_device = await db.device_description.find_one(
+        {"_id": ObjectId(oid=pg_device.mongo_id)})
+    if mongo_device is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='Specs not found. Notify us please about this request')
+    pg_dev = await models.DevicePoolFullInfo.from_tortoise_orm(pg_device)
+    return schemas.DevicePoolGet(**pg_dev.dict(), **mongo_device)
+
+
+@router.put('/{device_id}')
+async def update_device_by_id(device_id,
+                              device_info: models.DevicePoolPydantic,
+                              device_specs: schemas.DevicePoolPost = None):
+    pg_device = await models.DevicesPool.filter(id=device_id).first()
+    if pg_device is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Device not found')
 
     async with in_transaction():
-        async with mongo_client.start_session as s:
-            async with s.start_transaction():
-                new_device_info = await db.device_description.insert_one(
-                    jsonable_encoder(device_specs)
-                )
         device_info = device_info.dict()
-        device_info['mongo_id'] = new_device_info.inserted_id
-
-        await models.DevicesPool.create(**device_info.dict())
-
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=None)
+        await models.DevicesPool.filter(id=device_id).update(**device_info)
+        if device_specs is not None:
+            mongo_info = device_specs.dict()
+            await db.device_description.update_one(
+                {"_id": ObjectId(pg_device.mongo_id)}, {"$set": mongo_info})
+    return await get_device_by_id(device_id)
