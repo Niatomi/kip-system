2487f1e2300bd5f423b302b4e8c632c17ac4d0d9
Niatomi <playervoker@gmail.com>
2023-05-30 03:29:44 +0400

    Implemented view of ath-page

--- frontend/src/components/auth/auth-page.vue
+++ frontend/src/components/auth/auth-page.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="auth-page-container">
-    <div class="inputs-container">
+  <div class="auth-wrapper">
+    <div class="logo-container">
+      <img
+        :src="require('@/assets/logo/Logo-lighter.svg/')"
+        alt="Logo"
+        height="150"
+      />
+      <h1>КИП Геофизика</h1>
+      <svg width="400" height="5">
+        <rect width="500" height="5" style="fill: #d9d9d9" />
+      </svg>
+    </div>
+    <div class="auth-page-container">
       <h1>Вход</h1>
       <form action="" class="input-container">
         <input type="text" placeholder="Логин" />
@@ -18,36 +29,53 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.inputs-container {
-  background: #d9d9d9;
+.auth-wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+.logo-container {
+  margin-top: 3%;
+  margin-bottom: 5%;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .auth-page-container {
-  text-align: center;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: #d9d9d9;
+  padding-bottom: 3%;
+  width: 50%;
+  color: $secondary_textcolor;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  gap: 20px;
 }
 
 input {
-  appearance: none;
-  width: 100%;
-  border: 0;
+  border: none;
   height: 50px;
-  color: black;
-  font-size: 3rem;
-  padding-left: 10px;
+  font-size: 30px;
   border-radius: 20px;
-}
-.input-container {
-  display: flex;
-  gap: 5px;
-  flex-direction: column;
-  align-content: center;
-  align-items: center;
+  padding-left: 20px;
 }
 
-p {
-  background: rgb(189, 104, 104);
+button {
+  border: None;
+  font-size: 40px;
+  border-radius: 15px;
+  height: 80px;
+  color: $textcolor;
+  font-family: inherit;
+  background: #9ece9a;
 }
 </style>
