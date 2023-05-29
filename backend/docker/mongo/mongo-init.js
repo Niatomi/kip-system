3554ee41dbc3884c356d96b8740ed08cb4c697e7
Niatomi <playervoker@gmail.com>
2023-05-30 02:53:06 +0400

    Add test mongo db

--- backend/docker/mongo/mongo-init.js
+++ backend/docker/mongo/mongo-init.js
@@ -10,4 +10,12 @@ db.createUser({
 });
 db.createCollection("device_description");
 
+db = db.getSiblingDB("kip_system_db_test");
+db.createUser({
+  user: "api",
+  pwd: "api",
+  roles: [{ role: "readWrite", db: "kip_system_db_test" }],
+});
+db.createCollection("device_description");
+
 print("END #################################################################");
