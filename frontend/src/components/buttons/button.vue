7e74065978c70e0abb15c3f0d9be686d28218433
Niatomi <playervoker@gmail.com>
2023-05-30 20:13:53 +0400

    Implemented generic button component

---
+++ frontend/src/components/buttons/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="button-container" :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: ["bgColor", "textColor"],
+  computed: {
+    style() {
+      return "background: " + this.bgColor + "; color: " + this.textColor;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.button-container {
+  background: $color6;
+  color: $color8;
+  padding: 10px;
+  padding-left: 50px;
+  padding-right: 50px;
+  border-radius: 10px;
+}
+</style>
