906208ccd5d378599187ccdedd19b426adbec100
Niatomi <playervoker@gmail.com>
2023-05-30 20:15:45 +0400

    Implemented about company page

---
+++ frontend/src/views/home/about-page/about-page.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about-page-container">
+    <h1>КИП Геофизика</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repellat
+      adipisci ex veritatis nihil minima rerum doloribus, nostrum facere odio,
+      debitis labore quaerat dolorem pariatur sint quasi perferendis voluptatum
+      molestias!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error eos minima
+      sed hic tempora, reiciendis quod aperiam fugiat nisi tenetur fuga enim
+      tempore non doloribus vero ea magnam nam exercitationem. Impedit ipsa,
+      excepturi provident aliquid veniam esse iusto quod velit facere explicabo
+      officiis temporibus ex exercitationem libero itaque accusantium quaerat
+      voluptas voluptate odit mollitia similique quia magnam? Provident nobis
+      quisquam voluptatibus harum. Quaerat fugiat est repellat aspernatur ad qui
+      magni ea harum laborum quis odit ex totam alias consequuntur libero nam,
+      animi iure commodi? Explicabo earum a alias. Labore totam officiis eius
+      corporis necessitatibus in similique eligendi saepe, quas quo?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutPage",
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 40px;
+}
+.about-page-container {
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
