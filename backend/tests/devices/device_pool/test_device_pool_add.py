8679a9023b3f41105dd0b6f66b3759d4b2d41bb8
Niatomi <playervoker@gmail.com>
2023-06-01 12:10:51 +0400

    Add device_1 and 2

--- backend/tests/devices/device_pool/test_device_pool_add.py
+++ backend/tests/devices/device_pool/test_device_pool_add.py
@@ -6,26 +6,26 @@ from httpx import AsyncClient
 
 
 @pytest.mark.anyio
-async def test_device_post_ok(admin_client: AsyncClient, device: dict):
-    r = await admin_client.post('/device_pool/', json=device)
+async def test_device_post_ok(admin_client: AsyncClient, device_1: dict):
+    r = await admin_client.post('/device_pool/', json=device_1)
 
     assert r.status_code == 201
 
     device_from_db = await device_models.DevicesPool.filter(
-        name=device["device_info"]['name']).first()
+        name=device_1["device_info"]['name']).first()
 
     assert device_from_db is not None
-    device_from_db.name == device["device_info"]['name']
+    device_from_db.name == device_1["device_info"]['name']
 
     mongo_device = await db.device_description.find_one(
         {"_id": ObjectId(oid=device_from_db.mongo_id)})
     assert mongo_device is not None
-    assert mongo_device['specifications'] == device["device_specs"]['specifications']
+    assert mongo_device['specifications'] == device_1["device_specs"]['specifications']
 
 
 @pytest.mark.anyio
-async def test_device_duplicate_error(admin_client: AsyncClient, device: dict):
-    r = await admin_client.post('/device_pool/', json=device)
+async def test_device_duplicate_error(admin_client: AsyncClient, device_1: dict):
+    r = await admin_client.post('/device_pool/', json=device_1)
     assert r.status_code == 422
     msg: str = r.json()['detail'][0]['msg']
     assert msg.startswith('duplicate key value violates unique constraint')
---
+++ backend/tests/devices/devices_in_use/conftest.py
@@ -0,0 +1,100 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.fixture(scope="session")
+async def device_1():
+    return {
+        "device_specs": {
+            "description": "Description for device",
+            "specifications": [
+                {"spec1": "details"},
+                {"spec2": "details"},
+                {"spec3": "details"}
+            ]
+        },
+        "device_info": {
+            "name": "Device1",
+            "check_intervals": 123123,
+            "category": "category_1",
+            "price": 1000.01,
+            "resource": 123123,
+            "resource_of_useful_usage": 5
+        }
+    }
+
+
+@pytest.fixture(scope="session")
+async def device_2():
+    return {
+        "device_specs": {
+            "description": "Description for device",
+            "specifications": [
+                {"spec1": "details"},
+                {"spec2": "details"},
+                {"spec3": "details"}
+            ]
+        },
+        "device_info": {
+            "name": "Device2",
+            "check_intervals": 123123,
+            "category": "category_2",
+            "price": 1000.01,
+            "resource": 123123,
+            "resource_of_useful_usage": 5
+        }
+    }
+
+
+@pytest.fixture(scope="session")
+async def device_id_1(admin_client_with_devices_in_pool: AsyncClient):
+    r = await admin_client_with_devices_in_pool.get(
+        '/device_pool/device',
+        params={'name': 'Device1'}
+    )
+    return r.json()['id']
+
+
+@pytest.fixture(scope="session")
+async def device_id_2(admin_client_with_devices_in_pool: AsyncClient):
+    r = await admin_client_with_devices_in_pool.get(
+        '/device_pool/device',
+        params={'name': 'Device2'}
+    )
+    return r.json()['id']
+
+
+@pytest.fixture(scope="session")
+async def admin_client_with_devices_in_pool(admin_client: AsyncClient,
+                                            device_1: dict,
+                                            device_2: dict):
+
+    await admin_client.post('/device_pool/', json=device_1)
+    await admin_client.post('/device_pool/', json=device_2)
+
+    yield admin_client
+
+
+@pytest.fixture(scope="session")
+async def admin_client_with_active_devices(admin_client_with_devices_in_pool: AsyncClient,
+                                           device_id_1: dict,
+                                           device_id_2: dict):
+
+    device_1 = {
+        "invent_number": "123123",
+        "serial_number": "123123",
+        "place": "string1",
+        "device_id": device_id_1
+    }
+
+    device_2 = {
+        "invent_number": "string",
+        "serial_number": "string",
+        "place": "string2",
+        "device_id": device_id_2
+    }
+
+    await admin_client_with_devices_in_pool.post('/devices_in_use/', json=device_1)
+    await admin_client_with_devices_in_pool.post('/devices_in_use/', json=device_2)
+
+    yield admin_client_with_devices_in_pool
