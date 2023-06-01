894a9e055169bfe9a05a76fb67934dffd28e8f8a
Niatomi <playervoker@gmail.com>
2023-06-01 12:09:32 +0400

    pratially inmplemented ammortization logic

--- backend/src/devices/devices_in_use/service.py
+++ backend/src/devices/devices_in_use/service.py
@@ -1,3 +1,10 @@
-def calc_amortization():
+from .. import models
+
+
+def calc_amortization(device: models.DevicesPool):
+    yearly_metric_ammortization = 100 / device.resource_of_useful_usage
+    yearly_sum_ammortization = device.price * yearly_metric_ammortization
+    monthly_ammortization = yearly_sum_ammortization / 12
+
+    # add logic from clickhouse
     pass
-    # Годовая норма аммортизации = 100% / срок полезного использования
