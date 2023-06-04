264036bd2d74ff3a6a5802ea220873f33c2baab1
Niatomi <playervoker@gmail.com>
2023-06-05 03:05:46 +0400

    Implemented ammortization count

--- backend/src/devices/devices_in_use/service.py
+++ backend/src/devices/devices_in_use/service.py
@@ -1,10 +1,12 @@
+
 from .. import models
+from decimal import Decimal
 
 
-def calc_amortization(device: models.DevicesPool):
+def calc_amortization(device: models.DevicesPool, months_in_use):
     yearly_metric_ammortization = 100 / device.resource_of_useful_usage
-    yearly_sum_ammortization = device.price * yearly_metric_ammortization
+    yearly_sum_ammortization = (Decimal(device.price) *
+                                Decimal(yearly_metric_ammortization)) / Decimal(100)
     monthly_ammortization = yearly_sum_ammortization / 12
 
-    # add logic from clickhouse
-    pass
+    return monthly_ammortization * months_in_use
