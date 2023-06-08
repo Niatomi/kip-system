5bdeebe12c42c8de2a11d70d4f4d43673e54c16d
Niatomi <playervoker@gmail.com>
2023-06-08 22:46:16 +0400

    Initial commit

---
+++ frontend/src/vuex/device_pool/actions/api_requests.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+const qs = require('qs');
+
+export default {
+} 
---
+++ frontend/src/vuex/devices_in_use/devices_in_use_module.js
@@ -0,0 +1,17 @@
+import actions from "./actions/actions";
+import api_actions from "./actions/api_requests";
+import mutations from "./mutations/mutations";
+import getters from "./getters/getters";
+
+import { createStore, mapGetters } from "vuex";
+
+const const_actions = { ...actions, ...api_actions };
+
+let auth_module = createStore({
+  state: {},
+  mutations,
+  actions: const_actions,
+  getters,
+});
+
+export default auth_module;
