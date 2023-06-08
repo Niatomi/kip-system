9608dd40a098d95727cbe4b456951a02e3c4fbe9
Niatomi <playervoker@gmail.com>
2023-06-09 00:15:57 +0400

    Implemented 404 page

---
+++ frontend/src/views/404-page/404-error-page.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-error-container>
+        <div class="content-container">
+            <img
+              :src="require('@/assets/errors/404.svg/')"
+              alt="Logo"
+              height="300"
+            /> 
+            
+            <div class="text-container">
+                <h2>Что-то пошло не так....</h2>
+                <svg width="500" height="5">
+                    <rect width="500" height="5" style="fill: #d9d9d9" />
+                </svg>
+                <router-link to="/">
+                    <h1>На главную</h1>
+                </router-link>
+            </div>
+        </div>
+    </v-error-container>
+
+</template>
+
+<script>
+export default {
+    name: 'Error404Page',
+}
+</script>
+
+
+<style lang="scss">
+
+.text-container {
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    gap: 40px;
+    align-items: center;
+    flex-direction: column;
+}
+
+.content-container {
+    display: flex;
+    margin-top: 20vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.v-error-container {
+    height: 100vh;
+}
+
+</style>
