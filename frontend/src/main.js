2b3209c3d16d6dd98230062b35060fbb06ddcf23
Niatomi <playervoker@gmail.com>
2023-06-10 00:21:45 +0400

    Add material icons into use

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router/router";
 import store from "./vuex/authentication/auth_module";
 import { VueCookies } from "vue-cookies";
+import 'material-design-icons-iconfont'
 
 createApp(App).use(VueCookies, { expires: '7d'}).use(router).use(store).mount("#app");
