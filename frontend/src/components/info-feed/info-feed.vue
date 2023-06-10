4dcedece3c1b9af3b4fead2656c5a3e30469dc31
Niatomi <playervoker@gmail.com>
2023-06-11 01:05:38 +0400

    Add container for content

---
+++ frontend/src/components/info-feed/info-feed.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="feed-content-container">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InfoFeed'
+}
+</script>
+
+<style lang="scss">
+
+.feed-content-container {
+    display: flex;
+    background: $color2;
+    flex-direction: column;
+    align-items: center;
+    gap: 1%;
+    border-radius: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    height: fit-content;
+    width: 80%;
+}
+
+</style>
