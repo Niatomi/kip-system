a1a87b0d02c69d03716963d862012fa8945ac715
Niatomi <playervoker@gmail.com>
2023-06-11 01:06:12 +0400

    Implemented base show of active device

---
+++ frontend/src/components/devices/active-devices.vue
@@ -0,0 +1,89 @@
+<template>
+  <InfoFeed>
+    <div class="local-navbar">
+      <div class="btns">
+        <div class="btn btn__first">
+            <p>Все</p>
+        </div>
+        <div class="btn">
+            <p>Категория</p>
+        </div>
+        <div class="btn">
+            <p>Спецификация</p>
+        </div>
+        
+        <div class="btn">
+            <p>Статус</p>
+        </div>
+        <div class="btn">
+            <p>Место</p>
+        </div>
+        <div class="btn btn__last">
+            <p>Ответственный</p> 
+        </div>
+      </div>
+    </div>
+  </InfoFeed>
+</template>
+
+<script>
+import InfoFeed from '@/components/info-feed/info-feed.vue'
+
+export default {
+  name: 'ActiveDevices',
+  components: {
+      InfoFeed
+  }
+}
+</script>
+
+<style lang="scss">
+.local-navbar {
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  align-items: center;
+  width: 100% - 5;
+}
+
+.btns {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  flex-direction: row;
+}
+
+.btn { 
+
+  color: $color8;
+
+  background: $color6;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-left: solid $color7;
+  
+  
+  &__first {
+    border-left: none;
+    border-radius: 20px 0 0 20px;
+  }
+  
+  &__last {
+    border-radius: 0 20px 20px 0;
+  }
+  
+  &:hover {
+    background: $color7
+  }
+  
+  
+}
+
+</style>
--- frontend/src/views/chief-page/chief-main-page.vue
+++ frontend/src/views/chief-page/chief-main-page.vue
@@ -1,11 +1,59 @@
 <template>
-    <p>Страница для шефа</p>
+    <div class="chief-container">
+        <div class="menu-container">
+            <h4>Меню</h4>
+            <Button class="btn_override">Приборы</Button>
+            <Button class="btn_override">Сотрудники</Button>
+            <Button class="btn_override">БД Приборов</Button>
+        </div>
+        <ActiveDevices/>
+    </div>
 </template>
 
 <script>
+import Button from '@/components/buttons/button.vue'
+import ActiveDevices from '@/components/devices/active-devices'
+
 export default {
-    name: "ChiefPage"
+    name: "ChiefPage",
+    components: {
+        Button,
+        ActiveDevices
+    },
+    data() {
+        return {
+            
+        }
+    }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+
+
+.chief-container {
+    display: flex;
+    gap: 1%;
+}
+
+.menu-container {
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    background: $color2;
+    width: 20%;
+    gap: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    height: fit-content;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn_override {
+    width: 75%;
+    padding-left: 10px;
+}
+
+
+</style>
