e490ca935d1c171c5ca6ddbcc665ab35c8dfeae1
Niatomi <playervoker@gmail.com>
2023-06-11 02:47:23 +0400

    Add animations

--- frontend/src/views/chief-page/chief-main-page.vue
+++ frontend/src/views/chief-page/chief-main-page.vue
@@ -2,17 +2,20 @@
     <div class="chief-container">
         <div class="menu-container">
             <h4>Меню</h4>
-            <Button class="btn_override">Приборы</Button>
-            <Button class="btn_override">Сотрудники</Button>
-            <Button class="btn_override">БД Приборов</Button>
+            <Button class="btn_override" ref="Date.now()" @click="menu='activeDevices'">Приборы</Button>
+            <Button class="btn_override" ref="Date.now()" @click="menu='users'">Сотрудники</Button>
+            <Button class="btn_override" ref="Date.now()" @click="menu='devicePool'">БД Приборов</Button>
         </div>
-        <ActiveDevices/>
+        <Transition name="fade-slide" appear>
+            <ActiveDevices v-if="menu === 'activeDevices'"/>
+        </Transition>
     </div>
 </template>
 
 <script>
 import Button from '@/components/buttons/button.vue'
 import ActiveDevices from '@/components/devices/active-devices'
+import { Transition } from "vue";
 
 export default {
     name: "ChiefPage",
@@ -22,7 +25,7 @@ export default {
     },
     data() {
         return {
-            
+            menu: ''
         }
     }
 }
@@ -48,11 +51,26 @@ export default {
     height: fit-content;
     align-items: center;
     justify-content: center;
+    flex-grow: 0; 
+    flex-shrink: 0;
 }
 
 .btn_override {
     width: 75%;
     padding-left: 10px;
+}
+
+
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease-in-out;
+  transform: translateX(0px);
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateX(100px);
 }
 
 
--- frontend/src/views/home/home-page.vue
+++ frontend/src/views/home/home-page.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="home-page-container">
-    <img
+  <Transition name="fade-slide" appear>
+    <div class="home-page-container">
+      <img
       :src="require('@/assets/logo/Logo-lighter.svg/')"
       alt="Logo"
       height="300"
-    />
-    <h2>КИП Геофизика</h2>
-    <h2>Основана в 2077 году</h2>
-    <h2>Учитываем всё, но в основном приборы</h2>
-  </div>
+      />
+      <h2>КИП Геофизика</h2>
+      <h2>Основана в 2077 году</h2>
+      <h2>Учитываем всё, но в основном приборы</h2>
+    </div>
+  </Transition>
 </template>
+
+<script>
+import { Transition } from "vue";
+export default {}
+</script>
 
 <style lang="scss">
 .home-page-container {
@@ -18,5 +25,17 @@
   align-items: center;
   padding-top: 6%;
   gap: 30px;
+}
+
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: opacity 1s ease, transform 1s ease-in-out;
+  transform: translateY(0px);
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(-40px);
 }
 </style>
