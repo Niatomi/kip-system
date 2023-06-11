211708ba91407ea315846fc036966541dfc47bad
Niatomi <playervoker@gmail.com>
2023-06-12 00:37:16 +0400

    Add transitions

--- frontend/src/views/home/about-page/about-page.vue
+++ frontend/src/views/home/about-page/about-page.vue
@@ -1,25 +1,15 @@
 <template>
-  <div class="about-page-container">
-    <h1>КИП Геофизика</h1>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repellat
-      adipisci ex veritatis nihil minima rerum doloribus, nostrum facere odio,
-      debitis labore quaerat dolorem pariatur sint quasi perferendis voluptatum
-      molestias!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error eos minima
-      sed hic tempora, reiciendis quod aperiam fugiat nisi tenetur fuga enim
-      tempore non doloribus vero ea magnam nam exercitationem. Impedit ipsa,
-      excepturi provident aliquid veniam esse iusto quod velit facere explicabo
-      officiis temporibus ex exercitationem libero itaque accusantium quaerat
-      voluptas voluptate odit mollitia similique quia magnam? Provident nobis
-      quisquam voluptatibus harum. Quaerat fugiat est repellat aspernatur ad qui
-      magni ea harum laborum quis odit ex totam alias consequuntur libero nam,
-      animi iure commodi? Explicabo earum a alias. Labore totam officiis eius
-      corporis necessitatibus in similique eligendi saepe, quas quo?
-    </p>
-  </div>
+  <Transition name="fade-slide" appear>
+    <div class="about-page-container">
+      <h1>КИП Геофизика</h1>
+      <p>
+        Это система предназначенная для ведения учёта приборов в геофизике (но на самом деле довольно универсальная, её можно использовать в любых сферах, подобных по объектным областям)
+      </p>
+      <p>
+        Created by Niatomi
+      </p>
+    </div>
+  </Transition>
 </template>
 
 <script>
@@ -37,5 +27,17 @@ p {
   display: flex;
   flex-direction: column;
   gap: 20px;
+}
+
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: opacity 1s ease, transform 1s ease-in-out;
+  transform: translateY(0px);
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(40px);
 }
 </style>
--- frontend/src/views/home/job-page/job-page.vue
+++ frontend/src/views/home/job-page/job-page.vue
@@ -1,57 +1,60 @@
 <template>
-  <div class="job-page-container">
-    <div class="job-block">
-      <h2>Сатанист второго разряда</h2>
-      <h3>Зарплата: ))</h3>
-      <h4>
-        Описание: Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-        nisi autem ipsa, nemo minima sed praesentium voluptatem vitae in
-        reiciendis nihil possimus corrupti? Aperiam autem accusamus laborum
-        voluptatum nam. Nesciunt incidunt accusamus soluta porro. Veniam
-        provident harum autem saepe minima nihil dolore quam, quos hic explicabo
-        obcaecati distinctio, tempore reprehenderit rem odit animi iure dolorum
-        velit eum eligendi ipsa nisi. Sed fuga quos facere ipsum vel facilis
-        esse omnis molestias ipsa ex consequatur praesentium odit qui corrupti
-        distinctio quisquam odio dolore magnam fugit natus unde, ullam non
-        assumenda. Commodi quas at debitis optio temporibus, et ut explicabo
-        placeat officia illum!
-      </h4>
+  <Transition name="fade-slide" appear>
+  
+    <div class="job-page-container">
+      <div class="job-block">
+        <h2>Сатанист второго разряда</h2>
+        <h3>Зарплата: 15 000</h3>
+        <h4>
+          Описание: Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          nisi autem ipsa, nemo minima sed praesentium voluptatem vitae in
+          reiciendis nihil possimus corrupti? Aperiam autem accusamus laborum
+          voluptatum nam. Nesciunt incidunt accusamus soluta porro. Veniam
+          provident harum autem saepe minima nihil dolore quam, quos hic explicabo
+          obcaecati distinctio, tempore reprehenderit rem odit animi iure dolorum
+          velit eum eligendi ipsa nisi. Sed fuga quos facere ipsum vel facilis
+          esse omnis molestias ipsa ex consequatur praesentium odit qui corrupti
+          distinctio quisquam odio dolore magnam fugit natus unde, ullam non
+          assumenda. Commodi quas at debitis optio temporibus, et ut explicabo
+          placeat officia illum!
+        </h4>
+      </div>
+      <div class="job-block">
+        <h2>Пажилой лётчик истребитель</h2>
+        <h3>Зарплата: 54 000</h3>
+        <h4>
+          Описание: Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          nisi autem ipsa, nemo minima sed praesentium voluptatem vitae in
+          reiciendis nihil possimus corrupti? Aperiam autem accusamus laborum
+          voluptatum nam. Nesciunt incidunt accusamus soluta porro. Veniam
+          provident harum autem saepe minima nihil dolore quam, quos hic explicabo
+          obcaecati distinctio, tempore reprehenderit rem odit animi iure dolorum
+          velit eum eligendi ipsa nisi. Sed fuga quos facere ipsum vel facilis
+          esse omnis molestias ipsa ex consequatur praesentium odit qui corrupti
+          distinctio quisquam odio dolore magnam fugit natus unde, ullam non
+          assumenda. Commodi quas at debitis optio temporibus, et ut explicabo
+          placeat officia illum!
+        </h4>
+      </div>
+      <div class="job-block">
+        <h2>КИП Инженер</h2>
+        <h3>Зарплата: 50 000 </h3>
+        <h4>
+          Описание: Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          nisi autem ipsa, nemo minima sed praesentium voluptatem vitae in
+          reiciendis nihil possimus corrupti? Aperiam autem accusamus laborum
+          voluptatum nam. Nesciunt incidunt accusamus soluta porro. Veniam
+          provident harum autem saepe minima nihil dolore quam, quos hic explicabo
+          obcaecati distinctio, tempore reprehenderit rem odit animi iure dolorum
+          velit eum eligendi ipsa nisi. Sed fuga quos facere ipsum vel facilis
+          esse omnis molestias ipsa ex consequatur praesentium odit qui corrupti
+          distinctio quisquam odio dolore magnam fugit natus unde, ullam non
+          assumenda. Commodi quas at debitis optio temporibus, et ut explicabo
+          placeat officia illum!
+        </h4>
+      </div>
     </div>
-    <div class="job-block">
-      <h2>Пажилой лётчик истребитель</h2>
-      <h3>Зарплата: ))</h3>
-      <h4>
-        Описание: Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-        nisi autem ipsa, nemo minima sed praesentium voluptatem vitae in
-        reiciendis nihil possimus corrupti? Aperiam autem accusamus laborum
-        voluptatum nam. Nesciunt incidunt accusamus soluta porro. Veniam
-        provident harum autem saepe minima nihil dolore quam, quos hic explicabo
-        obcaecati distinctio, tempore reprehenderit rem odit animi iure dolorum
-        velit eum eligendi ipsa nisi. Sed fuga quos facere ipsum vel facilis
-        esse omnis molestias ipsa ex consequatur praesentium odit qui corrupti
-        distinctio quisquam odio dolore magnam fugit natus unde, ullam non
-        assumenda. Commodi quas at debitis optio temporibus, et ut explicabo
-        placeat officia illum!
-      </h4>
-    </div>
-    <div class="job-block">
-      <h2>КИП Инженер</h2>
-      <h3>Зарплата: ))</h3>
-      <h4>
-        Описание: Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-        nisi autem ipsa, nemo minima sed praesentium voluptatem vitae in
-        reiciendis nihil possimus corrupti? Aperiam autem accusamus laborum
-        voluptatum nam. Nesciunt incidunt accusamus soluta porro. Veniam
-        provident harum autem saepe minima nihil dolore quam, quos hic explicabo
-        obcaecati distinctio, tempore reprehenderit rem odit animi iure dolorum
-        velit eum eligendi ipsa nisi. Sed fuga quos facere ipsum vel facilis
-        esse omnis molestias ipsa ex consequatur praesentium odit qui corrupti
-        distinctio quisquam odio dolore magnam fugit natus unde, ullam non
-        assumenda. Commodi quas at debitis optio temporibus, et ut explicabo
-        placeat officia illum!
-      </h4>
-    </div>
-  </div>
+  </Transition>
 </template>
 
 <script>
@@ -87,5 +90,17 @@ export default {
   align-items: center;
   flex-direction: column;
   gap: 20px;
+}
+
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: opacity 1s ease, transform 1s ease-in-out;
+  transform: translateY(0px);
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(40px);
 }
 </style>
