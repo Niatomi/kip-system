1e83252c767e80719c512fa6fef1670479845f4b
Niatomi <playervoker@gmail.com>
2023-06-12 00:39:24 +0400

    Add find button component

---
+++ frontend/src/components/buttons/find-button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button type="submit"><h4>Найти</h4></button>
+</template>
+
+<script>
+export default {
+    name: 'FindButton'
+}
+</script>
+
+<style lang="scss">
+button {
+  border: None;
+  font-size: 30px;
+  border-radius: 0px 0px 20px 20px ;
+  color: $color8;
+  font-family: inherit;
+  width: 100%;
+  background: $color4;
+}
+
+</style>
