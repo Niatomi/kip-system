0fb6f93f23211817fd0bd330e4e316e61643d5f5
Niatomi <playervoker@gmail.com>
2023-06-13 12:04:02 +0400

    Changed show of device info popup

--- frontend/src/components/device/all-device-popup.vue
+++ frontend/src/components/device/all-device-popup.vue
@@ -8,16 +8,19 @@
     <p>Текущее место установки: {{ CHOSEN_DEVICE.place }}</p>
     <p>Инвентаризационный номер: {{ CHOSEN_DEVICE.invent_number }}</p>
     <p>Серийный номер: {{ CHOSEN_DEVICE.serial_number }}</p>
+    <p>Ресурс прибора к следующей поверке: {{ CHOSEN_DEVICE.resourceOnNextCheck }} {{ useYearVerb(CHOSEN_DEVICE.resourceOnNextCheck) }}</p>
+    <p>Время наработки: {{ convertTimeInUsage(CHOSEN_DEVICE.timeInUse) }}</p>
     <svg width="50%" height="5">
       <rect width="100%" height="5" style="fill: #d9d9d9" />
     </svg>
+    <p>Категория: {{ CHOSEN_DEVICE.category }}</p>
+    <p>Интервал поверки: {{ CHOSEN_DEVICE.check_intervals }} {{ useDayVerb(CHOSEN_DEVICE.check_intervals) }}</p>
+    <p>Стоимость: {{ getPrice }} ₽</p>
+    <p>Ресурс прибора: {{ CHOSEN_DEVICE.resource }} {{ useYearVerb(CHOSEN_DEVICE.resource) }}</p>
     <span>
       <p>Описание:</p>
       <p style="text-align: justify;">{{CHOSEN_DEVICE.description}}</p>
     </span>
-    <p>Категория: {{ CHOSEN_DEVICE.category }}</p>
-    <p>Интервал поверки (дни): {{ CHOSEN_DEVICE.check_intervals }}</p>
-    <p>Стоимость: {{ getPrice }} ₽</p>
     <table border="1">
       <caption>Спецификация устройства</caption>
       <tr>
@@ -40,6 +43,53 @@ import { toRaw } from 'vue'
 
 export default {
   name: 'AllDevicePopup',
+  methods: {
+    useYearVerb(valu) {
+      let val = valu % 10
+      let godaArr = [2, 3, 4]
+      let result = 'лет';
+      if (val === 1) {
+        result = 'год';
+      }
+      godaArr.forEach(value => {
+        if (val === value) {
+          result = 'года';
+        }
+      })
+      if (valu >= 5) {
+        result = 'лет'
+      }
+      return result;
+    },
+    useDayVerb(valu) {
+      let val = valu % 10
+      let daysArr = [2, 3, 4]
+      let result = 'дней'
+      if (val === 1) {
+        result = 'день';
+      }
+      daysArr.forEach(value => {
+        if (val === value) {
+          result = 'дня';
+        }
+      })
+      return result;
+    },
+    convertTimeInUsage(val) {
+      let seconds = Number(val);
+
+      if (seconds === 0) {
+        return 'Отсутствует'
+      }
+      let y = Math.floor(seconds / (3600*24*365));
+      let d = Math.floor((seconds - (y * (3600*24*365))) / (3600*24));
+
+      let year = y > 0 ? y + ' ' + this.useYearVerb(y) + ' ': ''
+      let days = d > 0 ? d + ' ' + this.useDayVerb(d) + ' ': ''
+
+      return year + days
+    }
+  },
   computed: {
     ...mapGetters([
       'CHOSEN_DEVICE'
@@ -61,6 +111,11 @@ export default {
       if (status === 'CHEKING') {
         return 'Поверяется'
       }
+      if (status === 'REGISTRED') {
+        return 'Зарегистрирован'
+      }
+      status = status.charAt(0) + status.toLowerCase().slice(1)
+      return status
     },
     getSpecs() {
       let ar = []
@@ -72,7 +127,7 @@ export default {
       })
       return ar
     },
-
+    
   },
 }
 </script>
