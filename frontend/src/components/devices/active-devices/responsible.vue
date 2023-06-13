9eea663f315e2263970464b77a53608bf57b9a07
Niatomi <playervoker@gmail.com>
2023-06-13 12:05:16 +0400

    Implemented sort by responsible

--- frontend/src/components/devices/active-devices/responsible.vue
+++ frontend/src/components/devices/active-devices/responsible.vue
@@ -1,8 +1,143 @@
 <template>
+  <div class="all-devices-container">
+    <InfoFeed>
+      <div class="inner-container">
+        <select v-model="selected">
+          <option value="" disabled hidden>Выберите отвественного за прибор</option>
+          <option v-for="responsible in AVAILABLE_RESPONSIBLES" :value="responsible" :key="Math.random()">
+            {{ responsible.full_name }}
+          </option>
+        </select>
+
+        <svg width="50%" height="5">
+          <rect width="100%" height="5" style="fill: #d9d9d9" />
+        </svg>
+      </div>
+      <Device
+      v-if="DEVICES_BY_RESPONSIBLE.length"
+      v-for="item in DEVICES_BY_RESPONSIBLE"
+      :key="item.id"
+      :device="item"
+      @click="openInfo(item.id)"
+      
+      />
+    </InfoFeed>
+    <Popup 
+    v-if="getPopupState" 
+    @closePopup="closePopup()"
+    >
+      <AllDevicePopup :key="Date.now()"/>
+    </Popup>
+  </div>
 </template>
 
 <script>
-export default {}
+import FindButton from '@/components/buttons/find-button'
+import InfoFeed from '@/components/info-feed/info-feed'
+import Button from '@/components/buttons/button'
+import Device from '@/components/device/device'
+import AllDevicePopup from '@/components/device/all-device-popup'
+import { mapActions, mapGetters } from 'vuex'
+import { toRaw } from 'vue'
+import Popup from '@/components/popup/popup'
+
+export default {
+  components: {
+    InfoFeed,
+    Button,
+    FindButton,
+    Device,
+    Popup,
+    AllDevicePopup
+  },
+  data() {
+    return {
+      popupActive: false,
+      selected: '',
+    }
+  },
+  watch: {
+    selected: function(val) {
+      this.GET_DEVICES_BY_RESPONSIBLE(val);
+    }
+  },
+  methods: {
+    ...mapActions([
+      'GET_DEVICE_FULL_INFO',
+      'GET_DEVICES_BY_RESPONSIBLE',
+      'GET_AVAILABLE_RESPONSIBLES_ACTION'
+    ]),
+    openInfo(itemId) {
+      this.GET_DEVICE_FULL_INFO(itemId).then(() => {
+         this.popupActive = true
+      })
+    },
+    closePopup() {
+      this.popupActive = false
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'AVAILABLE_RESPONSIBLES',
+      'DEVICES_BY_RESPONSIBLE'
+    ]),
+    getPopupState() {
+      return this.popupActive
+    },
+  },
+  mounted() {
+    this.GET_AVAILABLE_RESPONSIBLES_ACTION()
+  }
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.all-devices-container {
+  margin-bottom: 10px;
+}
+
+select {
+  width: 100%;
+  border-radius: 20px;
+  height: 45px;
+  font-size: 30px;
+  background: $color6;
+  font-family: inherit;
+  padding-left: 10px;
+  
+}
+
+option {
+  border-radius: 20px;
+}
+
+.inner-container {
+  width: 100%;
+  max-width: 97%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 10px;
+}
+
+.search {
+  color: $color8;
+  padding-left: 15px;
+  border: none;
+  border-radius: 20px;
+}
+
+input {
+  color: $color8;
+}
+
+</style>
