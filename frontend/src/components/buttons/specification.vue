bbf7fbe79d2051630eeabf2e21a2728f6f9fd5ac
Niatomi <playervoker@gmail.com>
2023-06-13 12:03:31 +0400

    Implemented sort by specifications

---
+++ frontend/src/components/buttons/specification.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="specification-container">
+    <h4><slot></slot></h4>
+    <button class="search_btn" @click="deleteSpecification()">
+      <i class="material-icons">close</i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Specification',
+  methods: {
+    deleteSpecification() {
+      this.$emit('delete')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.specification-container {
+  display: flex;
+  align-items: center;
+  background: $color6;
+  width: fit-content;
+  border-radius: 30px;
+  padding: 5px;
+  
+  gap: 10px;
+}
+
+h4 {
+  color: $color8;
+  padding-left: 10px;
+}
+
+.search_btn {
+  width: fit-content;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 100%;
+  background: $color7;
+}
+</style>
