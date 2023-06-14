7fe7132150d19ffe5dbb07aa7b5991396e300140
Niatomi <playervoker@gmail.com>
2023-06-14 22:43:38 +0400

    Add status form on device

--- frontend/src/components/device/all-device-popup.vue
+++ frontend/src/components/device/all-device-popup.vue
@@ -5,11 +5,25 @@
     </svg>
     <p>Текущая аммортизация: {{ fixedAmmortizationNumber }} ₽</p>
     <p>Текущий статус: {{ getStatus }}</p>
+    <p>Следующая поверка: {{ getCheckDate }}</p>
     <p>Текущее место установки: {{ CHOSEN_DEVICE.place }}</p>
     <p>Инвентаризационный номер: {{ CHOSEN_DEVICE.invent_number }}</p>
     <p>Серийный номер: {{ CHOSEN_DEVICE.serial_number }}</p>
     <p>Ресурс прибора к следующей поверке: {{ CHOSEN_DEVICE.resourceOnNextCheck }} {{ useYearVerb(CHOSEN_DEVICE.resourceOnNextCheck) }}</p>
     <p>Время наработки: {{ convertTimeInUsage(CHOSEN_DEVICE.timeInUse) }}</p>
+    <span>
+      <form class="input-container" @submit.prevent="addAction">
+        <h4>Применить статус</h4>
+        <select v-model="selectedAction">
+          <option value="" disabled hidden>Выберите статус</option>
+          <option v-for="status in getStatuses" :value="status" :key="Math.random()">
+            {{ status }}
+          </option>
+        </select>
+        <button type="submit" class="override_btn" :disabled="!selectedAction.length"><h4>Применить статус</h4></button>
+      </form>
+      <h4 v-if="response_msg.length">{{ response_msg }}</h4>
+    </span>
     <svg width="50%" height="5">
       <rect width="100%" height="5" style="fill: #d9d9d9" />
     </svg>
@@ -38,12 +52,50 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { toRaw } from 'vue'
+import dayjs from 'dayjs';
 
 export default {
   name: 'AllDevicePopup',
+  data() {
+    return {
+      checkDate: '',
+      statuses: [],
+      selectedAction: '',
+      response_msg: ''
+    }
+  },
   methods: {
+    ...mapActions([
+      'GET_DEVICE_NEXT_CHECK',
+      'GET_DEVICE_STATUSES',
+      'PATCH_NEW_DEVICE_STATUS'
+    ]),
+    addAction() {
+      let value = ''
+      if (this.selectedAction === 'Установлен') {
+        value = 'INSTALLED'
+      }
+      if (this.selectedAction === 'Поверен') {
+        value = 'CHECKED'
+      }
+      if (this.selectedAction === 'Починен') {
+        value = 'REPAIRED'
+      }
+      if (this.selectedAction === 'Поставлен на поверку') {
+        value = 'ON_CHECK'
+      }
+      let payload = {
+        'deviceId': this.CHOSEN_DEVICE.id,
+        'deviceStatus': value
+      }
+      this.PATCH_NEW_DEVICE_STATUS(payload).then(() => {
+        this.selectedAction = '',
+        this.response_msg = 'Статус применён'
+      })
+      this.$emit('updateEvent')
+    },
     useYearVerb(valu) {
       let val = valu % 10
       let godaArr = [2, 3, 4]
@@ -90,10 +142,46 @@ export default {
       return year + days
     }
   },
+  mounted() {
+    this.GET_DEVICE_NEXT_CHECK(this.CHOSEN_DEVICE.id).then((result) => {
+      this.checkDate = result
+    })
+    this.GET_DEVICE_STATUSES().then((result) => {
+      this.statuses = result
+    })
+  },
   computed: {
     ...mapGetters([
       'CHOSEN_DEVICE'
     ]),
+    getStatuses() {
+      const array = []
+      this.statuses.forEach(value => {
+        if (value === "INSTALLED") {
+          array.push('Установлен')
+        }
+        if (value === "CHECKED") {
+          array.push('Поверен')
+        }
+        if (value === "REPAIRED") {
+          array.push('Починен')
+        }
+        if (value === "ON_CHECK") {
+          array.push('Поставлен на поверку')
+        }
+      })
+      return array
+    },
+    getCheckDate() {
+      if (this.checkDate === null) {
+        return 'Требуется поверка'
+      } 
+      if (this.checkDate === 'NEEDS_CHECK') {
+        return 'Поверка просрочена'
+      } 
+      let chd = dayjs(this.checkDate)
+      return chd.format('DD.MM.YYYY')
+    },
     fixedAmmortizationNumber() {
       return parseFloat(this.CHOSEN_DEVICE.ammortization).toFixed(2);
     },
@@ -148,5 +236,36 @@ tr, td, th {
   padding: 5px;
   border: none;
   border-color: inherit;
+}
+
+.override_btn {
+  border-radius: 20px;
+
+  &:hover {
+    background: $color7;
+  }
+
+  &:disabled {
+    background: $color7;
+  }
+}
+
+select {
+  width: 100%;
+  height: 50px;
+  border-radius: 20px;
+  background: $color7;
+  border: none;
+  font-family: inherit;
+  font-size: inherit;
+  padding-left: 10px;
+}
+
+
+.input-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
